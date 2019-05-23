0f09a42e30321b13381ebf7aa9e71e8f87c04b60
JoshKarpel <karpel@wisc.edu>
2019-05-22 21:29:32 -0500

    resolves #2

--- notebooks/htc-tensorflow-notebook/start-htcondor.sh
+++ notebooks/htc-tensorflow-notebook/start-htcondor.sh
@@ -11,11 +11,11 @@ _MSG="Did NOT start HTCondor because using remote schedd"
 [ ! -v "_condor_SCHEDD_HOST" ] && if [ $(id -u) == 0 ] ; then
   _MSG="Started HTCondor via sudo"
   sudo -E -H -u $NB_USER PATH=$PATH -- mkdir -p "$_condor_local_dir/lock" "$_condor_local_dir/log" "$_condor_local_dir/run" "$_condor_local_dir/spool" "$_condor_local_dir/execute" "$_condor_local_dir/cred_dir"
-  sudo -E -H -u $NB_USER PATH=$PATH -- condor_master 
+  sudo -E -H -u $NB_USER PATH=$PATH -- condor_master
 else
   _MSG="Started HTCondor"
   mkdir -p "$_condor_local_dir/lock" "$_condor_local_dir/log" "$_condor_local_dir/run" "$_condor_local_dir/spool" "$_condor_local_dir/execute" "$_condor_local_dir/cred_dir"
-  condor_master 
+  condor_master
 fi
 
 echo $_MSG
