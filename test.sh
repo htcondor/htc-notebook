4e9d68c20ce228add3c3b7eda6fba4e28d9757ff
Josh Karpel <karpel@wisc.edu>
2020-04-20 12:43:25 -0500

    split config into personal and non-personal config; consolidate permission-settings commands

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ set -e
 
 CONTAINER_TAG=htc-notebook-test
 
-docker build -t ${CONTAINER_TAG} --file notebooks/htc-minimal-notebook/Dockerfile .
+docker build -t ${CONTAINER_TAG} notebooks/htc-minimal-notebook
 docker run -it --rm ${CONTAINER_TAG} bash
