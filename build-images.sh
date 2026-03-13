e56c234dd2e1d57dcde5f21617f66bf653b2dc20
Tim Theisen <tim@cs.wisc.edu>
2026-03-13 11:56:23 -0500

    HTCONDOR-3240: Add script to build and push images

---
+++ build-images.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Exit on any error
+set -e
+
+for i in $(<BASE_IMAGES.txt); do
+    NAME=${i##*/}
+    NAME=htc-${NAME%%:*}
+    echo "======= $NAME ======="
+    docker pull "$i"
+    docker image ls "htcondor/${NAME}" | grep htcondor > "${NAME}.out" || true
+    sed -i -e 's/  */:/' "${NAME}.out"
+    sed -i -e 's/ .*//' "${NAME}.out"
+    for IMAGE in $(<"${NAME}.out"); do
+        docker image rm "${IMAGE}"
+    done
+    docker build --tag "htcondor/${NAME}:latest" "notebooks/${NAME}" > "${NAME}.out" 2>&1
+    tail -n 1 "${NAME}.out"
+    VERSION=$(grep \$CondorVersion: "${NAME}.out")
+    VERSION=${VERSION#* }
+    VERSION=${VERSION%% *}
+    docker push "htcondor/${NAME}:${VERSION}"
+    docker push "htcondor/${NAME}:latest"
+done
